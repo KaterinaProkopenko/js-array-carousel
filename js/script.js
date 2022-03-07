986ed50046ad8ecbdd3e6655b4587e22086aed05
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-03-07 14:48:39 +0100

    Aggiornamento carousel js

--- js/script.js
+++ js/script.js
@@ -6,45 +6,94 @@ const items = [
     'img/05.jpg'
 ];
 
+const title = ['Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise'];
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+];
+
+
 let carouselImmages = '';
 
 for (let i = 0 ; i < items.length ; i++){
     carouselImmages += `
     <div class="img-container">
         <img src="${items[i]}" alt="Random picture">
-    </div>`
+    </div>`;
 }
+
+
+let titleElements = '';
+for (let i = 0 ; i < title.length ; i++){
+    titleElements = title[i];
+}
+
+const carouselTitleContainer = document.getElementById('title');
+carouselTitleContainer.innerHTML = titleElements;
 
 const carouselContainer = document.querySelector('div.left-img');
 carouselContainer.innerHTML += carouselImmages;
 
+
+
+
+
+
 const carouselElement = document.getElementsByClassName('img-container');
 const carouselSmallElement = document.getElementsByClassName('small-img');
+
+/* const carouselTestElement = document.getElementById('paragraph'); */
 
 carouselElement[0].classList.add('active');
 carouselSmallElement[0].classList.add('active-small');
 
-const buttonNext = document.querySelector('div#up');
+const buttonPrevious = document.querySelector('div#up');
 
 let activeElement = 0;
-let = activeSmallElement = 0;
-
-buttonNext.addEventListener('click', function(){
-    carouselElement[activeElement].classList.remove('active');
-    carouselSmallElement[activeSmallElement].classList.remove('active-small');
-    activeElement--;
-    activeSmallElement--;
-    carouselElement[activeElement].classList.add('active');
-    carouselSmallElement[activeSmallElement].classList.add('active-small');
-});
-
-const buttonPrevious = document.querySelector('div#down');
+let activeSmallElement = 0;
 
 buttonPrevious.addEventListener('click', function(){
     carouselElement[activeElement].classList.remove('active');
     carouselSmallElement[activeSmallElement].classList.remove('active-small');
-    activeElement++;
-    activeSmallElement++;
+    
+    if (activeElement === 0){
+        activeElement = items.length - 1;
+    } else {
+        activeElement--;
+    }
+
+    if (activeSmallElement === 0) {
+        activeSmallElement = items.length - 1;
+    } else {
+        activeSmallElement--;
+    }
+
+
+    carouselElement[activeElement].classList.add('active');
+    carouselSmallElement[activeSmallElement].classList.add('active-small');
+});
+
+const buttonNext = document.querySelector('div#down');
+
+buttonNext.addEventListener('click', function(){
+    carouselElement[activeElement].classList.remove('active');
+    carouselSmallElement[activeSmallElement].classList.remove('active-small');
+    if( activeElement === items.length - 1){
+        activeElement = 0;
+    } else {
+        activeElement++;
+    }
+
+    if (activeSmallElement === items.length - 1){
+        activeSmallElement = 0;
+    } else {
+        activeSmallElement++;
+    }
+
     carouselElement[activeElement].classList.add('active');
     carouselSmallElement[activeSmallElement].classList.add('active-small');
 });
